b80449b3abec9e2e3665e0fbdf1143c07e04b229
Maral Mashayekhi <mashayekhimaral@gmail.com>
2024-06-29 15:50:40 +0330

    "fix : slider completed"

--- Pages/sliderPage.js
+++ Pages/sliderPage.js
@@ -1,53 +1,55 @@
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
+import { router, routes } from "../main";
 
 export function sliderPage() {
   return `
-<div class="swiper w-full h-screen">
-  <div class="swiper-wrapper">
-    <!-- Slides -->
+<div class="swiper w-full h-3/5 text-center font-Roboto">
+  <div class="swiper-wrapper mb-16">
+
     <div class="swiper-slide">
-    
+     <img src="./imges/slide1.png" alt="slide1">
+     <p class="text-4xl mt-9">We provide high quality products just for you</p>
     </div>
-    <div class="swiper-slide">Slide 2</div>
-    <div class="swiper-slide">Slide 3</div>
-  </div>
-  <div class="swiper-pagination"></div>
+    <div class="swiper-slide">
+      <img src="./imges/slide2.png" alt="slide2">
+      <p class="text-4xl mt-9">Your satisfaction is our number one periority</p>
+    </div>
+    <div class="swiper-slide">
+      <img src="./imges/slide3.png" alt="slide3">
+      <p class="text-4xl mt-9">Let’s fulfill your fashion needs with shoearight now!</p>
+    </div>
 
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
+ </div>
 
-  <div class="swiper-scrollbar"></div>
+ <div class="swiper-pagination"></div>
+
+
+
 </div>
+
+<div class="text-center mt-14">
+<button id="next-btn" type="button" class="w-96 h-12 bg-darkBtn text-white font-Roboto rounded-3xl">Next</button>
+</div>
+
   `;
 }
 
-// export const swiper = new Swiper(".swiper", {
-//   // configure Swiper to use modules
-//   modules: [Navigation, Pagination],
-
-//   // Optional parameters
-//   direction: "vertical",
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//   },
+// document.addEventListener("DOMContentLoaded", function () {
+//   if (document.querySelector(".swiper")) {
+//     const swiper = new Swiper(".swiper", {
+//       modules: [Navigation, Pagination],
+//       // direction: "vertical",
+//       loop: true,
+//       pagination: {
+//         el: ".swiper-pagination",
+//       },
+//       navigation: {
+//         nextEl: "#next-btn",
+//       },
+//     });
+//   }
 // });
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
   if (document.querySelector(".swiper")) {
@@ -59,12 +61,17 @@ document.addEventListener("DOMContentLoaded", function () {
         el: ".swiper-pagination",
       },
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: "#next-btn",
       },
-      scrollbar: {
-        el: ".swiper-scrollbar",
-      },
+    });
+
+    swiper.on("slideChange", function () {
+      if (swiper.activeIndex === 2) {
+        document.querySelector("#next-btn").textContent = "Get Started";
+        document.querySelector("#next-btn").addEventListener("click", () => {
+          router.navigate(routes.login);
+        });
+      }
     });
   }
 });
