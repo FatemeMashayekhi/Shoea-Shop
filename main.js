0914c9c2671487b04d9367fdb77bd019e04d7265
Maral Mashayekhi <mashayekhimaral@gmail.com>
2024-07-05 21:25:55 +0330

    fix : cart added

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ import { mainLayout } from "./Layouts/main-layout/mainLayout";
 import { getWishes, wishListPage } from "./Pages/wishListPage";
 import { productDetailsPage } from "./Pages/productDetailsPage";
 import { searchPage, searching } from "./Pages/searchPage";
-import { cartPage } from "./Pages/cartPage";
+import { cartPage, getCart } from "./Pages/cartPage";
 import { secondLayout } from "./Layouts/secondary-layout/secondLayout";
 
 export const router = new Navigo("/");
@@ -56,7 +56,7 @@ router
     const { html, createEventListeners } = await productDetailsPage(match);
     render(html, createEventListeners);
   })
-  .on(routes.cart, () => render(secondLayout(cartPage())))
+  .on(routes.cart, () => render(secondLayout(cartPage(), getCart())))
   .on(routes.wishList, () => render(wishListPage(), getWishes))
   .on(routes.search, (match) => render(searchPage(), searching(match.params)))
   .resolve();
