ae0eac388ff187fe1e4025b3d0b56046245268d2
Maral Mashayekhi <mashayekhimaral@gmail.com>
2024-07-08 15:49:16 +0330

    feat : add nav to html not layout

--- Pages/cartPage.js
+++ Pages/cartPage.js
@@ -3,7 +3,8 @@ import axios from "../api";
 export function cartPage() {
   return `
 
-  <div class="bg-lightGray flex flex-col font-Roboto p-6 gap-y-8">
+  <div class="min-h-screen flex flex-col font-Roboto">
+  <div class="bg-lightGray flex flex-col font-Roboto p-6 gap-y-8 grow">
     <div class="flex justify-between items-center">
       <div class="flex gap-x-5 items-center">
         <img src="./public/imges/logo.png" alt="logo" class="w-4" />
@@ -13,7 +14,7 @@ export function cartPage() {
     <label for="search-box" class="relative">
       <i id="search" class="fas fa-search absolute top-4 left-3 text-btnListBg text-xl cursor-pointer"></i>
       <input type="text" id="search-input" class="w-full h-14 bg-productsBg px-11 rounded-2xl placeholder-placeholderText" placeholder="Search.." />
-  </label>
+    </label>
     <div id="cart-container" class="flex flex-col gap-y-6">
       <div class="flex bg-white rounded-3xl p-6 gap-x-6">
         <div class="bg-navBg rounded-2xl size-20">
@@ -41,6 +42,31 @@ export function cartPage() {
         </div>
       </div>
     </div>
+  </div>
+   <footer class="sticky bottom-0 z-40 bg-white"">
+   <div class="flex justify-evenly mb-3 text-center items-center">
+    <a href="/products" data-navigo>
+      <img src="./public/imges/home.png" alt="home" />
+      <p class="text-10">Home</p>
+    </a>
+    <a href="/cart" data-navigo>
+      <img src="./public/imges/cart.png" alt="cart" />
+      <p class="text-10">Cart</p>
+    </a>
+    <a href="#" data-navigo>
+      <img src="./public/imges/shoppi.png" alt="order" />
+      <p class="text-10">Orders</p>
+    </a>
+    <a href="#" data-navigo>
+      <img src="./public/imges/wallet.png" alt="wallet"/>
+      <p class="text-10">Wallet</p>
+    </a>
+    <a href="#" data-navigo>
+      <img src="./public/imges/profile.png" alt="profile"/>
+      <p class="text-10">Profile</p>
+    </a>
+   </div>
+ </footer>
   </div>
 
   `;
