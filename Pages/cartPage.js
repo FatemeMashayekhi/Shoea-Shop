27e020a08bb40b4278ce5bcf0640eb3ab2203878
pourya-asrzad <poriaasrzad@gmail.com>
2024-07-09 12:23:00 +0330

    fix : cart

--- Pages/cartPage.js
+++ Pages/cartPage.js
@@ -2,44 +2,15 @@ import axios from "../api";
 
 export async function cartPage() {
   const cart = await getCart();
-  console.log(cart);
 
-  const filteredProducts = searchQuery
-    ? cart.filter((item) => item.name.toLowerCase().includes(searchQuery))
-    : cart;
-
-  document.getElementById("cart-container").innerHTML = "";
-  filteredProducts.forEach((item) => {
-    document.getElementById("cart-container").innerHTML += `
-<div id="card" class="flex bg-white rounded-3xl p-6 gap-x-6">
-<div class="bg-navBg rounded-2xl w-48 h-28 flex justify-center items-center">
-<img src="${item.imgUrl}" alt="${item.name}" class="top-10 mix-blend-darken">
-</div>
-<div class="flex flex-col gap-y-3 w-full">
-<div class="flex justify-between">
-<p class="font-semibold">${item.name}</p>
-<img src="./public/imges/bin.png" alt="trash-icon" data-productId="${item.id}" id="trash-icon" class="w-5 h-6 cursor-pointer">
-</div>
-<div class="flex gap-x-3 items-center text-xs text-textGray">
-<span style="background-color:${item.colorCode};" class="rounded-full size-4"></span>
-<span>${item.color}</span>
-<span>|</span>
-<span>Size = ${item.sizes}</span>
-</div>
-<div class="flex items-center justify-between">
-<p id="single-price" class="font-semibold text-lg">$${item.price}</p>
-<div class="bg-navBg rounded-3xl w-24 p-2 font-semibold flex justify-center gap-x-4 items-center text-sm">
-<i id="minus" class="fa-solid fa-minus cursor-pointer"></i>
-<span id="quantity">${item.quantity}</span>
-<i id="plus" class="fa-solid fa-plus cursor-pointer"></i>
-</div>
-</div>
-</div>
-</div>
-`;
-  });
-
-  const createEventListeners = () => {};
+  const createEventListeners = () => {
+    console.log(document.querySelectorAll(".plus"));
+    document.querySelectorAll("#plus").forEach((plusElement) => {
+      plusElement.addEventListener("click", () => {
+        console.log("plus button clicked");
+      });
+    });
+  };
   //   const searchInput = document.getElementById("search-input");
   //   searchInput.addEventListener("input", () => {
   //     const searchQuery = searchInput.value.toLowerCase();
@@ -75,31 +46,37 @@ export async function cartPage() {
       <input type="text" id="search-input" class="w-full h-14 bg-productsBg px-11 rounded-2xl placeholder-placeholderText" placeholder="Search.." />
     </label>
     <div id="cart-container" class="flex flex-col gap-y-6">
-      <div class="flex bg-white rounded-3xl p-6 gap-x-6">
-        <div class="bg-navBg rounded-2xl size-20">
-          <img src="./public/imges/adidas/Miniaturized/1.png" alt="pic" class="">
+     ${cart
+       .map((item) => {
+         return `
+        <div id="card" class="flex bg-white rounded-3xl p-6 gap-x-6">
+        <div class="bg-navBg rounded-2xl w-48 h-28 flex justify-center items-center">
+        <img src="${item.imgUrl}" alt="${item.name}" class="top-10 mix-blend-darken">
         </div>
         <div class="flex flex-col gap-y-3 w-full">
-          <div class="flex justify-between">
-            <p class="font-semibold">Air Jordan 3 Retro</p>
-            <img src="./public/imges/bin.png" alt="trash-icon" class="w-5 h-6">
-          </div>
-          <div class="flex gap-x-3 items-center text-xs text-textGray">
-            <span class="bg-black rounded-full size-4"></span>
-            <span>Black</span>
-            <span>|</span>
-            <span>Size = 42</span>
-          </div>
-          <div class="flex items-center justify-between">
-            <p class="font-semibold text-lg">$105.00</p>
-            <div class="bg-navBg rounded-3xl w-24 p-2 font-semibold flex justify-center gap-x-4 items-center text-sm">
-              <i class="fa-solid fa-minus cursor-pointer"></i>
-              <span>2</span>
-              <i class="fa-solid fa-plus cursor-pointer"></i>
-            </div>
-          </div>
+        <div class="flex justify-between">
+        <p class="font-semibold">${item.name}</p>
+        <img src="./public/imges/bin.png" alt="trash-icon" data-productId="${item.id}" id="trash-icon" class="w-5 h-6 cursor-pointer">
         </div>
-      </div>
+        <div class="flex gap-x-3 items-center text-xs text-textGray">
+        <span style="background-color:${item.colorCode};" class="rounded-full size-4"></span>
+        <span>${item.color}</span>
+        <span>|</span>
+        <span>Size = ${item.sizes}</span>
+        </div>
+        <div class="flex items-center justify-between">
+        <p id="single-price" class="font-semibold text-lg">$${item.price}</p>
+        <div class="bg-navBg rounded-3xl w-24 p-2 font-semibold flex justify-center gap-x-4 items-center text-sm">
+        <i id="minus" class="fa-solid fa-minus cursor-pointer"></i>
+        <span id="quantity">${item.quantity}</span>
+        <i id="plus"  class="fa-solid fa-plus cursor-pointer"></i>
+        </div>
+        </div>
+        </div>
+        </div>
+        `;
+       })
+       .join("")}
     </div>
   </div>
 <footer class="sticky bottom-0 z-40 bg-white">
