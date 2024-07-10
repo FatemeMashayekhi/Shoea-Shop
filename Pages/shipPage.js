979cd5b05240fe7dcb3184d3d86fdd15ecdaa63c
Maral Mashayekhi <mashayekhimaral@gmail.com>
2024-07-10 13:00:30 +0330

    fix : get shipment

--- Pages/shipPage.js
+++ Pages/shipPage.js
@@ -1,7 +1,24 @@
+import axios from "../api";
+
 export async function shipPage() {
+  const shipment = await getShipType();
+  console.log(shipment);
+
   const createEventListeners = () => {};
 
   const html = ``;
 
   return { html, createEventListeners };
 }
+
+const getShipType = async () => {
+  try {
+    const response = await axios.get("/shipment");
+    if (response.status === 200) {
+      const shipment = response.data;
+      return shipment;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
